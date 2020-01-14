10e81604bb8f0305d8b3c7144a838247f0a16add
Gleb Kletskov <gfgfddgleb@gmail.com>
2020-01-15 01:38:50 +0300

    Add start command handling in bot

--- app/bot/commands/commands.ts
+++ app/bot/commands/commands.ts
@@ -2,10 +2,12 @@
 
 import courseCommand from './course';
 import helpCommand from './help';
+import start from './start';
 
 function registerCommands(bot) {
   helpCommand(bot);
   courseCommand(bot);
+  start(bot);
 }
 
 export default registerCommands;
---
+++ app/bot/commands/start.ts
@@ -0,0 +1,38 @@
+import TelegramBot = require('node-telegram-bot-api');
+import { IUser } from 'app/server/models/IUser';
+import fetch from 'node-fetch';
+import config from '../../config';
+
+function start(bot: TelegramBot) {
+  bot.onText(/\/start/, (msg, match) => {
+    const chatId = msg.chat.id;
+
+    const  newUser: IUser  = {
+      first_name: msg.from.first_name,
+      id: msg.from.id,
+      last_name: msg.from.last_name,
+      username: msg.from.username,
+      wallets: [],
+    };
+
+    fetch(`http://localhost:${config.PORT}/start`, {
+      method: 'post',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then(res => res.json())
+      .then(res => {
+        bot.sendMessage(chatId, 'you was registred');
+      })
+      .catch(err => {
+        console.log(err);
+        bot.sendMessage(chatId, 'Sorry, where was an error: ' + err);
+      });
+    // const helpString = '';
+    // bot.sendMessage(chatId, helpString);
+  });
+}
+
+export default start;
