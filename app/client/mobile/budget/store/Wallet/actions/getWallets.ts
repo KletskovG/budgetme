4f98429b33f977aa2ee170551b195474d833c23c
Gleb Kletskov <gfgfddgleb@gmail.com>
2020-04-05 14:29:37 +0300

    Change route for wallets

--- app/client/mobile/budget/store/Wallet/actions/getWallets.ts
+++ app/client/mobile/budget/store/Wallet/actions/getWallets.ts
@@ -9,13 +9,18 @@ export function getWalletsAction() {
   return async (dispatch: Dispatch<any>) => {
     const email = await AsyncStorage.getItem('@email');
     dispatch(getWalletsPending());
+    console.log(`${config.baseUrl}/user/${email}/wallet`);
     return fetch(`${config.baseUrl}/user/${email}/wallet`)
       .then(res => res.json())
       .then(json =>  {
+        console.log(json)
+        console.log('SUCCESS;')
         dispatch(getWalletsSuccess(json));
         return json
       })
       .catch((error: Error) => {
+        console.log('ERROr');
+        console.log(error)
         dispatch(getWalletsError(error));
       });
   }
